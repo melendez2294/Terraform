4b3622c8b6de53eae8c469206e9268f9c2fba356
GarrettC777 <garrett.church.820@my.csun.edu>
2019-11-11 18:00:57 -0800

    s3 added

--- Terraform/IAM/iam_users.tf
+++ Terraform/IAM/iam_users.tf
@@ -1,3 +1,12 @@
+terraform {
+    backend  "s3" {
+    region         = "us-west-2"
+    bucket         = "cit480-thegroup"
+    key            = "ec2/terraform.tfstate" 
+    dynamodb_table = "tf-state-lock"
+    }
+} 
+
 provider "aws" {
   region		  = "us-west-2"
   profile		  = "default"
