5e1fb1efce3996aae54a9c4dbba1b483c032c214
Garrett Church <garrett.church.820@my.csun.edu>
2019-11-15 11:44:05 -0800

    added Lambda start/stop scripts.

---
+++ Lambda/lambda_Start.py
@@ -0,0 +1,9 @@
+import boto3
+region = 'us-west-2'
+instances = ['i-05fd9af471d656818', 'i-0ec2572d224f67d62']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
+
---
+++ Lambda/lambda_Stop.py
@@ -0,0 +1,8 @@
+import boto3
+region = 'us-west-2'
+instances = ['i-05fd9af471d656818', 'i-0ec2572d224f67d62']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
